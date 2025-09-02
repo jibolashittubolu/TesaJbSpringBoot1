84899237291a4f60fb76136453513e016cb834af
jibolashittubolu <54463012+jibolashittubolu@users.noreply.github.com>
2025-09-02 23:59:50 +0100

    Update StudentController.java

--- src/main/java/com/example/tesajbdemo/controller/StudentController.java
+++ src/main/java/com/example/tesajbdemo/controller/StudentController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collector;
